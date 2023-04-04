e25eb7ee449a3943c784c573de214cc2ca5c3340
Evgeny Bessonnitsyn <81041213+Turukmokto@users.noreply.github.com>
2023-04-04 23:49:43 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,15 +3,20 @@ import numpy as np
 
 from scipy.stats import norm
 
+chat_id = 1112700607  # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+
+def ans(power, l, shift):
+    return (l - shift * norm.ppf(power)) * 2 / 50 ** 2
+
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    a = 1 - p
+    l = x.mean()
+    shift = np.sqrt(np.var(x)) / np.sqrt(len(x))
+    return ans(1 - a / 2, l, shift), \
+           ans(a / 2, l, shift)
+
