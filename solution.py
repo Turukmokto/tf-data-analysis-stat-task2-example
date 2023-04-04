6833684cf897beae7fda6148103b8070d5db9a70
Evgeny Bessonnitsyn <81041213+Turukmokto@users.noreply.github.com>
2023-04-05 00:27:34 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,21 +1,17 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import gamma
 
 chat_id = 1112700607  # Ваш chat ID, не меняйте название переменной
 
-
-def ans(power, l, shift):
-    return (l - shift * norm.ppf(power)) * 2 / 50 ** 2
-
+def ans(a, l, n):
+    return (l + gamma.ppf(a / 2, n) / n - 1 / 2) * 2 / 50 ** 2
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    a = 1 - p
     l = x.mean()
-    shift = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return ans(1 - a / 2, l, shift), ans(a / 2, l, shift)
-
+    n = x.size
+    return ans(1 + p, l, n), ans(1 - p, l, n)
