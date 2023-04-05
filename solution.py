7bfbff1eeee7f99089d6a91b0f6012d2d9c278ed
Evgeny Bessonnitsyn <81041213+Turukmokto@users.noreply.github.com>
2023-04-05 18:06:43 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,15 +5,15 @@ from scipy.stats import gamma
 
 chat_id = 1112700607  # Ваш chat ID, не меняйте название переменной
 
-def ans(a, l, n):
-    return (l - gamma(1, n).ppf(a) / n - 1 / 2) * 2 / 50 ** 2
+def ans(al, l, n):
+    return (l + gamma.ppf(al / 2, a=n) / n - 1 / 2) * 2 / 50 ** 2
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    a = 1 - p
+    al = 1 - p
     l = x.mean()
     n = x.size
-    return ans(1 - a / 2, l, n), ans(a / 2, l, n)
+    return ans(al / 2, l, n), ans(1 - al / 2, l, n)
 
